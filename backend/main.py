5f4b3b9c98f5da3018b2ecd50f180108dd290d99
Saad Babar <saadmb@umich.edu>
2024-03-23 04:59:57 -0400

    restoring old main.py

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,11 @@
 print("Hello World")
+<<<<<<< HEAD
 # practicing commite changes
 
 x = 4
 print (x * x)
 for i in range(4):
     print(x)
+=======
+# practicing commite changes
+>>>>>>> f78625c (practicing updating code)
